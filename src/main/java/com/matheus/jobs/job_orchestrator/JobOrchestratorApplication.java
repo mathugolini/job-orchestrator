29ff9d6ed579d31ca0d30a1d8f65d17b077fcfcf
mathugolini <mathugolini@gmail.com>
2026-02-27 23:54:07 -0300

    feat: initialize job orchestrator domain and project structure

---
+++ src/main/java/com/matheus/jobs/job_orchestrator/JobOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.jobs.job_orchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobOrchestratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheus/jobs/job_orchestrator/JobOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.jobs.job_orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
